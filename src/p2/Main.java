e6834181546227c96ba02b8ad6b682aaedd2bf96
Niklas <menkniklas@gmail.com>
2026-05-27 16:13:02 +0200

    Uebungsblatt 2 Algorithmen hinzugefuegt

--- src/p2/Main.java
+++ src/p2/Main.java
@@ -8,6 +8,26 @@ public class Main {
         if (n == 1 || n == 0) return 1;
         return n * fakultaet(n-1);
     } // seems to work.
-
-
+    public static int sumArray(int[] a){
+        int sum = 0;
+        for (int i = 1; i <= a.length; i++) { // in der VL ab 1
+            sum += a[i];
+        }
+        return sum;
+    }
+    public static int sumArrayR(int[] a, int n){
+        if(n==1) return a[n];
+        else return a[n] + sumArrayR(a, n-1);
+    }
+    public static int methodeMitK(int[] a, int m){
+        int k = 0;
+        int total = 0;
+        for (int i = 0; i <=a.length ; i++) {
+            if (total + a[i] <= m){
+                k++;                //Index gesucht!
+                total = total+ a[i];
+            }else break;
+        }
+        return k;
+    }
 }
